105798e26d798ecee3d1b651e63e4454e6fcea2e
Masood ul Rehman <masooddulrehman@gmail.com>
2024-09-01 07:56:49 +0500

    created pages for workspace, updated sidebar links, styles

--- src/app/workspace/components/sidebar.tsx
+++ src/app/workspace/components/sidebar.tsx
@@ -15,14 +15,15 @@ import More from "@/components/icons/more";
 import LaterNormal from "@/components/icons/later-normal";
 import LaterFilled from "@/components/icons/latter-filled";
 import UserWorkspaces from "./user-workspaces";
+import Link from "next/link";
 
 const Sidebar = () => {
   const pathname = usePathname();
+  const currentpath = pathname.split("/");
   const [selected, setSelected] = useState<
     "home" | "dms" | "activity" | "later" | "more"
   >("home");
   useEffect(() => {
-    const currentpath = pathname.split("/");
     if (currentpath[3]) {
       if (currentpath[3] == "dms") setSelected("dms");
       else if (currentpath[3] == "activity") setSelected("activity");
@@ -34,30 +35,47 @@ const Sidebar = () => {
     <div className="bg-slack_dark_bg w-[80px] text-white h-[calc(100vh-36px)] py-6 px-2 flex flex-col justify-between items-center">
       <div className="flex flex-col  gap-4">
         <UserWorkspaces />
-        <div className="mt-4 flex flex-col items-center gap-2 cursor-pointer">
+        <Link
+          href={`/workspace/${currentpath[2]}`}
+          className="mt-4 flex flex-col items-center gap-2 cursor-pointer"
+          onClick={() => setSelected("home")}
+        >
           <div
-            className={cn(selected == "home" && "bg-accent/20 p-2  rounded-lg")}
+            className={cn(
+              "w-8 h-8 flex items-center justify-center",
+              selected == "home" && "bg-accent/20   rounded-lg  "
+            )}
           >
-            <div className="w-5 h-5">
+            <div className="w-5 h-5 ">
               {selected == "home" ? <HomeFilled /> : <HomeNormal />}
             </div>
           </div>
           <h4 className="text-xs font-bold">Home</h4>
-        </div>
-        <div className="mt-4 flex flex-col items-center gap-2 cursor-pointer">
+        </Link>
+        <Link
+          href={`/workspace/${currentpath[2]}/dms`}
+          className="mt-4 flex flex-col items-center gap-2 cursor-pointer"
+        >
           <div
-            className={cn(selected == "dms" && "bg-accent/20 p-2  rounded-lg")}
+            className={cn(
+              "w-8 h-8 flex items-center justify-center",
+              selected == "dms" && "bg-accent/20  rounded-lg"
+            )}
           >
             <div className="w-5 h-5">
               {selected == "dms" ? <DmFilled /> : <DmNormal />}
             </div>
           </div>
           <h4 className="text-xs font-bold">DMs</h4>
-        </div>
-        <div className="mt-4 flex flex-col items-center gap-2 cursor-pointer">
+        </Link>
+        <Link
+          href={`/workspace/${currentpath[2]}/activity`}
+          className="mt-4 flex flex-col items-center gap-2 cursor-pointer"
+        >
           <div
             className={cn(
-              selected == "activity" && "bg-accent/20 p-2  rounded-lg"
+              "w-8 h-8 flex items-center justify-center",
+              selected == "activity" && "bg-accent/20   rounded-lg"
             )}
           >
             <div className="w-5 h-5">
@@ -65,12 +83,16 @@ const Sidebar = () => {
             </div>
           </div>
           <h4 className="text-xs font-bold">Activity</h4>
-        </div>
+        </Link>
 
-        <div className="mt-4 flex flex-col items-center gap-2 cursor-pointer">
+        <Link
+          href={`/workspace/${currentpath[2]}/later`}
+          className="mt-4 flex flex-col items-center gap-2 cursor-pointer"
+        >
           <div
             className={cn(
-              selected == "later" && "bg-accent/20 p-2  rounded-lg"
+              "w-8 h-8 flex items-center justify-center",
+              selected == "later" && "bg-accent/20   rounded-lg"
             )}
           >
             <div className="w-5 h-5">
@@ -78,10 +100,15 @@ const Sidebar = () => {
             </div>
           </div>
           <h4 className="text-xs font-bold">Later</h4>
-        </div>
+        </Link>
         <div className="mt-4 flex flex-col items-center gap-2 cursor-pointer">
           <div
-            className={cn(selected == "more" && "bg-accent/20 p-2  rounded-lg")}
+            className={cn(
+              "w-8 h-8 flex items-center justify-center",
+
+              selected == "more" && "bg-accent/20   rounded-lg"
+            )}
+            onClick={() => setSelected("more")}
           >
             <div className="w-5 h-5">
               <More />
