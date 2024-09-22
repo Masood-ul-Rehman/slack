ff5b4826001d4b0a493b7316035a418478fd6be8
Masood ul Rehman <masooddulrehman@gmail.com>
2024-09-23 01:20:42 +0500

    added emoji provider

--- src/components/reactions.tsx
+++ src/components/reactions.tsx
@@ -1,9 +1,13 @@
+import { MdOutlineAddReaction } from "react-icons/md";
+import { cn } from "@/lib/utils";
+
+import Hint from "./hint";
+import EmojiPopover from "./emoji-popover";
+import { Button } from "./ui/button";
 import { ReactionsProps } from "@/features/workspaces/types";
-import { Id } from "@/convex/_generated/dataModel";
 import useWorkspaceId from "@/features/workspaces/hooks/use-workspace-Id";
 import { useGetCurrentMember } from "@/features/workspaces/api/members/use-current-member";
-import { Button } from "./ui/button";
-import { cn } from "@/lib/utils";
+
 export const Reactions = ({ data, onChange }: ReactionsProps) => {
   const { workspaceId } = useWorkspaceId();
   const currentMember: any = useGetCurrentMember({ workspaceId });
@@ -11,21 +15,36 @@ export const Reactions = ({ data, onChange }: ReactionsProps) => {
   return (
     <div className="flex items-center gap-1 my-1">
       {data.map((reaction) => (
-        <Button
+        <Hint
           key={reaction._id}
-          className={cn(
-            "flex items-center gap-1 h-6 px-2 rounded-full bg-slate-200/70 border border-transparent text-slate-700 hover:bg-slate-200",
-            reaction.memberIds.includes(currentMember.data?.result?._id) &&
-              "bg-blue-100/70 border-blue-500 text-blue"
-          )}
-          onClick={() => {
-            onChange(reaction.reaction);
-          }}
+          label={`${reaction.count} ${reaction.count === 1 ? "person" : "people"} reacted with ${reaction.reaction}`}
         >
-          <span>{reaction.reaction}</span>
-          <span>{reaction.count}</span>
-        </Button>
+          <Button
+            key={reaction._id}
+            className={cn(
+              "flex items-center gap-1 h-6 px-2 rounded-full bg-slate-200/70 border border-transparent text-slate-700 hover:bg-slate-200 text-muted-foreground",
+              reaction.memberIds.includes(currentMember.data?.result?._id) &&
+                "bg-blue-100/70 border-blue-500 text-blue"
+            )}
+            onClick={() => {
+              onChange(reaction.reaction);
+            }}
+          >
+            {reaction.reaction}
+            <span className="text-xs font-semibold">{reaction.count}</span>
+          </Button>
+        </Hint>
       ))}
+      <EmojiPopover
+        hint="Add reaction"
+        onEmojiSelect={(emoji: string) => {
+          onChange(emoji);
+        }}
+      >
+        <button className="h-7 px-3 rounded-full bg-slate-200/70 border border-transparent text-slate-700 hover:bg-slate-200 text-muted-foreground">
+          <MdOutlineAddReaction className="size-4" />
+        </button>
+      </EmojiPopover>
     </div>
   );
 };
